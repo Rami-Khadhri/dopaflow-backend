ebef64812557b53d8253cb0c662d7055e6811e3a
Rami-khadhri <ramikhd5@gmail.com>
2025-05-04 01:30:52 +0100

    version 1.0.1

--- src/main/java/crm/dopaflow_backend/DTO/KeyIndicatorsDTO.java
+++ src/main/java/crm/dopaflow_backend/DTO/KeyIndicatorsDTO.java
@@ -13,7 +13,7 @@ public class KeyIndicatorsDTO {
     private BigDecimal totalOpportunityValue; // Total value of WON opportunities
     private long newOpportunities; // Count of IN_PROGRESS opportunities
     private long completedTasks; // Count of Done tasks
-    private double customerSatisfaction; // Mocked value
+    private long totalOpportunitiesForUser;
     private long newCompanies;
     private long newContacts;
 
